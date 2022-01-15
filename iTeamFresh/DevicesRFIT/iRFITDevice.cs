d483ae617127105b0306206d927f9ba5af7dec75
mrBasil <killla_666@mail.ru>
2022-01-15 21:27:31 +0500

    Создал интерфейс для работы с RFIT считывателями

---
+++ iTeamFresh/DevicesRFIT/iRFITDevice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTeamFresh.DevicesRFIT
+{
+    internal interface iRFITDevice
+    {
+        string Marker { get; set; }
+    }
+}
